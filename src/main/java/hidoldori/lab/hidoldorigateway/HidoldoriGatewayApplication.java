bdc970078d62aa77141b59f17cb41ada636e1fb1
hidoldori <hidoldori@gmail.com>
2021-01-11 13:16:55 +0900

    all

---
+++ src/main/java/hidoldori/lab/hidoldorigateway/HidoldoriGatewayApplication.java
@@ -0,0 +1,13 @@
+package hidoldori.lab.hidoldorigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HidoldoriGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HidoldoriGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hidoldori/lab/hidoldorigateway/HidoldoriGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package hidoldori.lab.hidoldorigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HidoldoriGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
